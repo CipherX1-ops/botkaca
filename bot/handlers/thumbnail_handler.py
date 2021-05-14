3947fb8bc6c1463dc4433177b233d9affff95d96
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-15 02:33:41 +0430

    Update thumbnail_handler.py

--- bot/handlers/thumbnail_handler.py
+++ bot/handlers/thumbnail_handler.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, Message, Filters
+from pyrogram import Client, filters 
+from pyrogram.types import Message
 from os.path import join as os_path_join
 from bot import COMMAND, LOCAL, CONFIG
 from bot.plugins import thumbnail_video
